936be901d00693ef0ebb6500fc021ac400f8ab67
Valerie Boem <vboem@gmail.com>
2017-02-18 16:40:10 -0500

    Completed seciton 7

--- ajax.js
+++ ajax.js
@@ -1,17 +1,34 @@
 $(document).ready(function () {
-  $('button').click(function() {
+  $('#step12 button').on('click', function (){
+   $.ajax({
+    method: 'GET',
+    url: 'http://first-ajax-api.herokuapp.com/',
+    dataType:''
+    });
+  });
+  $('#step3456 button').click(function() {
     $.ajax({
-      url: 'http://first-ajax-api.herokuapp.com/pong',
+      url: 'http://first-ajax-api.herokuapp.com/ping',
       method: 'GET',
       dataType: 'text'
-    }).done(function (responseData) {
-      $('#step3456').append('Wow what a ride! I came all the way from the server!')
-      console.log('Ajax is done loading the page!');
+    }).done(function (data) {
+      console.log(data);
+      console.log('Ajax is done loading the page');
+      $('#step3456').append(data);
     }).fail(function () {
-      $('#step3456').append("Sorry, the /pong page isn't loading now. Try again soon!")
+      $('#step3456').append(data)
       console.log('Hey! The request to load this page has failed');
     }).always(function () {
-      console.log('Hey! The request is finished');
+      console.log('The request is finished');
     })
+  });
+  $('#step7 button').click(function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      dataType: 'text'
+    }).done(function (data) {
+      console.log(data);
+    });
   });
 });
