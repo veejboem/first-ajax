edcd5cf0eadf6fd294c524787446806dbd8bcd73
Valerie Boem <vboem@gmail.com>
2017-02-18 16:08:31 -0500

    SCompleted section 5

--- ajax.js
+++ ajax.js
@@ -15,4 +15,18 @@ $(document).ready(function () {
       $('#step3456').append('Wow what a ride! I came all the way from the server!');
     });
   });
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/pong',
+      method: 'GET',
+      dataType: 'text'
+    }).fail(function () {
+      $('#step3456').append("Sorry, the /pong page isn't loading now. Try again soon!");
+    });
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'text'
+  }).always(function () {
+    console.log('Hey the request is finished');
+  });
 });
